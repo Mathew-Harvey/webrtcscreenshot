e9dd80e06a4733281805720a2d726a91afc0ad14
Mat <mathewharvey@gmail.com>
2021-11-17 14:22:14 +0800

    3

--- app.js
+++ app.js
@@ -11,4 +11,4 @@ router.get('/',function(req,res){
   app.use('/', router);
   app.listen(process.env.PORT|| 3000);
   
-  console.log('Running at Port 3001'); 
+  console.log('Running at Port 3000'); 
